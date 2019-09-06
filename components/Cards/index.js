984df05dee3943d9e11e2504e0821d60584dddbe
ericbanker12 <ericbanker12@gmail.com>
2019-09-06 10:52:43 -0500

    add cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,32 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then(obj=>{
+    //console.log(obj)
+    let cardsContainer = document.querySelector('.cards-container')
+    for (let topic of Object.values(obj.data.articles)) {
+        let articles = topic.map(createCard)
+        cardsContainer.append(...articles)
+    }
+})
+
+function createCard(article) {
+    // card
+    let card = createElement('div', {className:'card'})
+    // headline
+    let headline = createElement('div',{className:'headline', textContent:article.headline})
+    // author
+    let author = createElement('div',{className:'author'})
+    // img-container
+    let imgContainer = createElement('div',{className:'img-container'})
+    // img
+    let img = createElement('img', {src:article.authorPhoto})
+    imgContainer.appendChild(img)
+    // span
+    let span = createElement('span', {textContent:`By ${article.authorName}`})
+    author.append(imgContainer, span)
+    card.append(headline, author)
+
+    return card
+}
